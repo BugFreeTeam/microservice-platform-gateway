927766688235489b314b8754bdbe733416cd2d75
15927249176 <961924346@qq.com>
2020-07-14 21:33:14 +0800

    网关

---
+++ src/main/java/com/anjuxing/platform/gateway/MicroservicePlatformGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.anjuxing.platform.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class MicroservicePlatformGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicePlatformGatewayApplication.class, args);
+    }
+
+}
